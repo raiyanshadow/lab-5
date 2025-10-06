be226726ded409c13ca5feef3d46e4334eeba71d
raiyanshadow <taseenraiyan@gmail.com>
2025-10-06 17:06:34 -0400

    added token functionaity

--- src/main/java/example/GetToken.java
+++ src/main/java/example/GetToken.java
@@ -1,5 +1,7 @@
 package example;
 
+import java.io.IOException;
+
 /**
  * Class for getting your Grade API token.
  */
@@ -9,13 +11,14 @@ public class GetToken {
      * Run to get your token after you complete the code.
      * @param args unused arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         GetExample example = new GetExample();
 
-        String gradeAPISignUpURL = "https://grade-apis.panchen.ca/signUp";
+        String gradeAPISignUpURL = "https://grade-apis.panchen.ca/signUp?username=raiyan";
         // TODO Task 1: use the run method to get your Grade API token (or obtain your token through a web browser).
         String response = "";
-        // response = example.run( ...)
+        response = example.run(gradeAPISignUpURL);
         System.out.println(response);
+
     }
 }
